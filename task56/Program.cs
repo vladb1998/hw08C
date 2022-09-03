b4b641f8cab539785aa0dd50663462988a00278d
vlad <vladbelyvskij@gmail.com>
2022-09-03 23:17:28 +0300

    task 56

---
+++ task56/Program.cs
@@ -0,0 +1,64 @@
+﻿//Метод создания массива
+void Massif(int[,] array)
+{
+    const int indent = 3;
+    for (int line = 0; line < array.GetLength(0); line++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            array[line, column] = new Random().Next(1, 30);
+            Console.Write($"{array[line, column],indent}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Метод подсчета суммы в каждой строке массива и вывод номера строки с наименьшей суммой
+void SumInLine(int[,] array)
+{
+
+    //Подсчет суммы элементов в строке
+    int line = 0;
+    int sum = 0;
+
+    int sizeArrayMin = array.GetLength(0);
+    int[] ArrayMin = new int[sizeArrayMin];
+
+    while (line < array.GetLength(0))
+    {
+
+        for (int column = 0; column < array.GetLength(0); column++)
+        {
+            sum = sum + array[line, column];
+        }
+
+        ArrayMin[line] = sum;
+
+        Console.WriteLine($"Сумма строки {line} - {sum}");
+        Console.WriteLine();
+
+        line++;
+        sum = 0;
+    }
+
+    // Поиск минимальной суммы
+    int minNUmber = ArrayMin[0];
+    int index = 0;
+    for (int count = 0; count < sizeArrayMin; count++)
+    {
+        if (minNUmber > ArrayMin[count])
+        {
+            minNUmber = ArrayMin[count];
+            index = count;
+        }
+    }
+    Console.WriteLine($"В строке {index} минимальная сумма {minNUmber}");
+
+}
+
+//Создаем массива и вызываем методы
+int[,] arr = new int[4, 4];
+Massif(arr);
+SumInLine(arr);
+
