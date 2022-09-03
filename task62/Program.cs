d23b4250ca0bd024dc54f3fb3303efeb7310bb14
vlad <vladbelyvskij@gmail.com>
2022-09-03 23:18:46 +0300

    task 62

---
+++ task62/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создаем массив 4х4
+int[,] spiral = new int[4, 4];
+
+//Переменная для задания шага при заполнении
+int step = new Random().Next(1, 20);
+
+const int indent = 3;
+
+//Цикл заполнения массива по спирали
+for (int line = 0; line < spiral.GetLength(0); line++)
+{
+    for (int column = 0; column < spiral.GetLength(0); column++)
+    {
+        if (line == 0) { spiral[line, column] = step; step++; }
+        if (line == 1 & column < 3) { spiral[line, column] = step + 7; step++; }
+        if (line == 1 & column == 3) { spiral[line, column] = step - 3; }
+        if (line == 2 & column == 0) { spiral[line, column] = step + 3; }
+        if (line == 2 & column > 0 & column < 3) { spiral[line, column] = step + 8; step--; }
+        if (line == 2 & column == 3) { spiral[line, column] = step; }
+        if (line == 3 & column <= 4) { spiral[line, column] = step + 4; step--; }
+
+        Console.Write($"{spiral[line, column],indent} ");
+    }
+    Console.WriteLine();
+}
